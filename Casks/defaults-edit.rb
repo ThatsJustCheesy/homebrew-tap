c6b1b624c8dcf25391f9dc60ec795e46ffa0cf73
Ian Gregory <justcheesy@outlook.com>
2022-10-15 22:19:08 -0400

    Fix download link

--- Casks/defaults-edit.rb
+++ Casks/defaults-edit.rb
@@ -1,9 +1,9 @@
 cask "defaults-edit" do
   version "0.4.0"
+  tagged_version = "#{version}_1"
   sha256 "9ee9ebffb4f9d07450f901502d10a48e82b5f01f6a47d1ca462ad17e19c64373"
 
-  url "https://github.com/ThatsJustCheesy/defaults-edit/releases/download/v#{version}/defaults-edit.v#{version}.zip",
-      verified: "https://github.com/ThatsJustCheesy/defaults-edit"
+  url "https://github.com/ThatsJustCheesy/defaults-edit/releases/download/v#{tagged_version}/defaults-edit.v#{version}.zip"
 
   name "defaults edit"
   desc "Graphical user defaults editor"
